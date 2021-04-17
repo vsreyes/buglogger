ac03139f0c52c1c3221c1bb68d17bb345b8f7762
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-17 20:26:22 +0200

    Logitem setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import LogItem from './LogItem';
 
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
-}
+  const [logs, setLogs] = useState([]);
 
-export default App
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map(log => (
+            <LogItem />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default App;
