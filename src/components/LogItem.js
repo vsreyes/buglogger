5428fe0e0c130c0d89da09448b057054e335e714
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-18 11:34:30 +0200

    Layout setup

--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 
-const LogItem = () => {
+const LogItem = ({ log: { _id, priority, user, text, created } }) => {
+  const setVariant = () => {
+    if (priority === 'high') {
+      return 'danger';
+    } else if (priority === 'moderate') {
+      return 'warning';
+    } else {
+      return 'success';
+    }
+  };
+
   return (
     <tr>
-      <td>1</td>
-      <td>1</td>
-      <td>1</td>
-      <td>1</td>
-      <td>1</td>
+      <td>
+        <Badge variant={setVariant()} className='p-2'>
+          {priority.charAt(0).toUpperCase() + priority.slice(1)}
+        </Badge>
+      </td>
+      <td>{text}</td>
+      <td>{user}</td>
+      <td>{created}</td>
+      <td>
+        <Button variant='danger' size='sm'>
+          x
+        </Button>
+      </td>
     </tr>
   );
 };
